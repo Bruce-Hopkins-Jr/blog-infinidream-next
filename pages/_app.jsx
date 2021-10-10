55c15c09f12280c7e461b1009a270a44a4af21c3
Bruce-Hopkins-Jr <behopkinsjr@gmail.com>
2021-10-10 20:13:27 +0000

    added GA

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -12,7 +12,26 @@ import "../stylesheets/admin-styles/posts.css"
 import "../stylesheets/admin-styles/uploadImages.css"
 
 
+import { useRouter } from 'next/router';
+import React, {useEffect} from 'react'
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', 'UA-190645408-2', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+
   return <Component {...pageProps} />
 }
 export default MyApp
